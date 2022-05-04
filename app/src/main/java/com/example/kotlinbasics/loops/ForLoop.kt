017eb32c1196132d3bc7f021a89bc2827d26b282
Vitalijus <vitalijus.kolinko@develappers.de>
2022-05-04 16:01:48 +0300

    For loop was added

---
+++ app/src/main/java/com/example/kotlinbasics/loops/ForLoop.kt
@@ -0,0 +1,57 @@
+package com.example.kotlinbasics.loops
+
+fun main() {
+
+    val array1 = arrayOf(1, 2, 3, 4, 5)
+    for (i in array1) {
+        println(i)
+    }
+
+    val array2 = arrayOfNulls<Int>(101)
+    for (i in 0..array2.size - 1) {
+        array2[i] = i
+    }
+    // or
+    for (i in 0 until array2.size) {
+        array2[i] = i
+    }
+    // or
+    for (i in array2.indices) {
+        array2[i] = i
+    }
+    for (i in array2) {
+        println(i)
+    }
+
+    for (i in 100 downTo 0 step 3) {
+        println(i)
+    }
+
+    val arrayOfNums = arrayOfNulls<Int>(101)
+    for ((index, i) in (200..300).withIndex()) {
+        arrayOfNums[index] = i
+    }
+    for (i in arrayOfNums) {
+        println(i)
+    }
+    for ((index, i) in arrayOfNums.withIndex()) {
+        arrayOfNums[index] = i?.times(2)
+    }
+    for (i in arrayOfNums) {
+        println(i)
+    }
+
+    val arrayOfNumbers = arrayOfNulls<Int>(301)
+    for ((index, i) in (600 downTo 300).withIndex()) {
+        arrayOfNumbers[index] = i
+    }
+    for (i in arrayOfNumbers) {
+        if (i?.rem(5) == 0) {
+            println(i)
+        }
+    }
+    // or
+    for (i in arrayOfNumbers.size - 1 downTo 0 step 5) {
+        println(i)
+    }
+}
